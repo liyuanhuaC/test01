e30f4b70d5f4fce7567a8666c870cb1f96033432
zs <zs@bjpowernode.com>
2021-07-11 14:16:05 +0800

    第三次提交

--- src/com/kmu/User.java
+++ src/com/kmu/User.java
@@ -4,4 +4,8 @@ public class User {
     private int age;//年龄
     private String name;
     private int phone;
+
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
 }
